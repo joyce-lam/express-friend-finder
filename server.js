5fc554f77285dec73442ad786650564e0fa8ad5e
Joyce <joycelamsaungan@gmail.com>
2018-02-27 20:55:52 -0800

    modifying functions

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
 //Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path");
 
 //sets up express app
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 //sets up express app to handle data parsing
 app.use(bodyParser.unlencoded({ extended: false}));
 app.user(bodyParser.json());
 
-var friends = [];
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
 
-//routes
-//basic route that sends the user first to the AJAX page
-app.get("/", function(req, res) {
-	res.sendFile(path.join(__dirname, "home.html"));
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
 });
 
