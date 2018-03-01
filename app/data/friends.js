e20a9dfef3ffe06fdecd825ec9ff8c0717cb7aae
Joyce <joycelamsaungan@gmail.com>
2018-02-28 23:56:40 -0800

    fixing functions

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,20 +2,20 @@ var friendsArray = [
 	{
 		name: "girl",
 		photo: "http://static.telegram.wtf/i/stickers/catana_comics/CAADAQAD8AEAAgz26wgcmm6PWzEtAQI.png",
-		scores:["1",
-				"2",
-				"2",
-				"3",
-				"5"]
+		scores:[1,
+				2,
+				2,
+				3,
+				5]
 	},
 	{
 		name: "guy",
 		photo: "http://static.telegram.wtf/i/stickers/catana_comics/CAADAQADKgIAAgz26wj3G5ySdYsYQgI.png",
-		scores:["4",
-				"4",
-				"1",
-				"2",
-				"5"]
+		scores:[4,
+				4,
+				1,
+				2,
+				5]
 	}];
 
 
